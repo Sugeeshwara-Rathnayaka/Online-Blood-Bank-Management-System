534a3ca004f1d7589cdcafb7f0adbe426765b3c9
Sugeeshwara-Rathnayaka <sugeeshwara01@gmail.com>
2025-04-13 00:22:47 +0530

    Create Dashboard
    Change main.jsx and app.jsx

---
+++ blood-bank-project/dashboard/src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useContext, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Context } from "./main";
+import axios from "axios";
+import "./App.css";
+
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Messages from "./components/Messages";
+import Doctors from "./components/Doctors";
+import AddNewDoctor from "./components/AddNewDoctor";
+import AddNewAdmin from "./components/AddNewAdmin";
+import Sidebar from "./components/Sidebar";
+
+const App = () => {
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(Context);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/admin/me",
+          { withCredentials: true }
+        );
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+        // eslint-disable-next-line no-unused-vars
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated]);
+  return (
+    <>
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/doctor/addnew" element={<AddNewDoctor />} />
+          <Route path="/admin/addnew" element={<AddNewAdmin />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/doctors" element={<Doctors />} />
+        </Routes>
+        <ToastContainer position="top-center" autoClose={3000} />
+      </Router>
+    </>
+  );
+};
+
+export default App;
